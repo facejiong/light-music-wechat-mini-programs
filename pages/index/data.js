fdf0e77adaeca534a9547de19cd8337c2c498dff
facejiong <2279763407@qq.com>
2017-04-11 19:28:28 +0800

    feat: it is working well

---
+++ pages/index/data.js
@@ -0,0 +1,83 @@
+export default [
+      {
+        src:'http://dl.stream.qqmusic.qq.com/C4000039ca2a2wTGTP.m4a?vkey=C143595FEDB5445287196C2F1AEBC3A962AB62F92750397EE75470ED6386A593C9A01FF23E3F78136FE6772D368C9D9F6B0DE9E3EF18FD29&guid=2943454662&uin=2279763407&fromtag=66',
+        poster: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000004CDl3Z0LfA0V.jpg?max_age=2592000',
+        name:'Muyuuka',
+        author: 'RURUTIA'
+      }, {
+        src:'http://dl.stream.qqmusic.qq.com/C400002D9dGu3JxQCY.m4a?vkey=739894D640919CE02A3D08FDE86A76EFB2502A689FE564385930E27AEF9566C5C86F4D1084AF2DFADF2C4B4DA19993C528D4BBD1F21182E0&guid=2943454662&uin=2279763407&fromtag=66',
+        poster: 'https://y.gtimg.cn/music/photo_new/T002R300x300M0000001IDYP1MwhCZ.jpg?max_age=2592000',
+        name: '时间的魔法',
+        author: 'Leaf',
+      }, {
+        src: 'http://dl.stream.qqmusic.qq.com/C400000VqWGW4bDU9u.m4a?vkey=00B3393126FA49D95870059242BDA5406246AF4C61431D88A7CC3B7457C1CBA186B229EECBB273D5BD470EFEBE6560C58291ED85FC3D3FFB&guid=2943454662&uin=2279763407&fromtag=66',
+        poster: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000003vakYm0rpNYr.jpg?max_age=2592000',
+        name: '晚安了今天',
+        author: 'MOSAIC'
+      }, {
+        src: 'http://dl.stream.qqmusic.qq.com/C400004LmbVD2qNWbz.m4a?vkey=89C89418379C3AAD1396A80161662479CE6F2F9E46AB7E634A18F9AE45F3E95CAD31B9759A2CF5B188F23368BE99533C1A6016EE63857FF6&guid=2943454662&uin=2279763407&fromtag=66',
+        poster: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000001w2CWK3hzN6r.jpg?max_age=2592000',
+        name: '晚安娜娜',
+        author: '平野義久'
+      }, {
+        src: 'http://dl.stream.qqmusic.qq.com/C400001N4Ots19SsDU.m4a?vkey=92C7D6FC25563E4CE34C926C3C7DCD1FB4C180B365907E6AFA332216174DA4D7CF12A67FC19C87D09BE8FDC506BC45E93A21B2D668A56F55&guid=2943454662&uin=2279763407&fromtag=66',
+        poster: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000002Uflih088lIb.jpg?max_age=2592000',
+        name: '羽翼',
+        author: '折戸伸治'
+      }, {
+        src: 'http://dl.stream.qqmusic.qq.com/C400000VRDOJ0MLLur.m4a?vkey=0761FA4513C4EE941D7C9F0CB537522E176804FE8B47DB11EA06A8EA8A443FEB8E6BE45AAD3FB74613AF22B5F5F94F29D6FAF43656D54C1F&guid=2943454662&uin=2279763407&fromtag=66',
+        poster: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000000Xkryn0vUqzL.jpg?max_age=2592000',
+        name: 'Real',
+        author: 'Capo Productions'
+      }, {
+        src: 'http://dl.stream.qqmusic.qq.com/C400003EvMgK2bwx6m.m4a?vkey=73D30B1989C10ABDADD222CE404E564C83B6F50A28EAF55E778F63C180B0D24B25A3A4095870AEE4EA58443D518B63496974E0F832DD8886&guid=2943454662&uin=2279763407&fromtag=66',
+        poster: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000002I9dvU4FT2Sp.jpg?max_age=2592000',
+        name: '盛夏的一天',
+        author: '久石譲'
+      }, {
+        src: 'http://dl.stream.qqmusic.qq.com/C400002Yxsnd2Dg3Ji.m4a?vkey=4F5141748FEF7416B22D4D2A86111E040B9B15AB4DC46B7667410C73C42A03DECB26DC74B450199E2AC0294C33910B94EF516495C76C865F&guid=2943454662&uin=2279763407&fromtag=66',
+        poster: 'https://y.gtimg.cn/music/photo_new/T002R300x300M0000025bdDj2DmMX3.jpg?max_age=2592000',
+        name: 'Love is the Answer',
+        author: 'Sofia Kallgren'
+      }, {
+        src: 'http://dl.stream.qqmusic.qq.com/C400003yxI3n0FjZyW.m4a?vkey=BF246BB2708331F4B001C9B867D3F309CC9A80A46F741999A916A862D975FA45494816C76889EC10C1F5DDBF9F8EB4CA1B64D228C01FDD3F&guid=2943454662&uin=2279763407&fromtag=66',
+        poster: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000004BCUwt376Y6M.jpg?max_age=2592000',
+        name: 'Life',
+        author: 'Tobu'
+      }, {
+        src: 'http://dl.stream.qqmusic.qq.com/C400004KO4DX4SLkRR.m4a?vkey=1066A9B9310DCFAD1D7C5EFA3BF92E378614DFEDC842C8E5FD22057A6AB9653A3B522CA7405C87CB85CB660F262BCDDB72757224DE2A3437&guid=2943454662&uin=2279763407&fromtag=66',
+        poster: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000001DZx4l0sNYgi.jpg?max_age=2592000',
+        name: 'L',
+        author: '几米'
+      }, {
+        src: 'http://dl.stream.qqmusic.qq.com/C400004AhQtz18fBQ6.m4a?vkey=3E0BA334059676800707B46AEFF94BA42319282ADD1236ECFAF26D510CF7AA073C1200C30E2A2802DE07E255CF61B44A1E62E4DB732C42D1&guid=2943454662&uin=2279763407&fromtag=66',
+        poster: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000004VwwhZ3NxocT.jpg?max_age=2592000',
+        name: 'Intro',
+        author: '박지윤 (朴志胤)'
+      }, {
+        src: 'http://dl.stream.qqmusic.qq.com/C400001WVCCl0xOCgc.m4a?vkey=6DC79D10EF217951D3DD7EE3979E2154FE7616944125984837969B6B05D8BB608DCB92489890D867BE9ABD4A602793BF3E9E0E31C7CB0F8A&guid=2943454662&uin=2279763407&fromtag=66',
+        poster: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000002sb1r11kz1ot.jpg?max_age=2592000',
+        name: 'Hoppipolla',
+        author: 'Sigur Rós'
+      }, {
+        src: 'http://dl.stream.qqmusic.qq.com/C400003TFu2P42gaJw.m4a?vkey=4DE07616715F1207D2A1A124A42267075ADC4FCCE9E7518CF208056BFDA8C4777DAABAEDC1BAB2AA9D7677CA989C6A7A03DC5A2627056D61&guid=2943454662&uin=2279763407&fromtag=66',
+        poster: 'https://y.gtimg.cn/music/photo_new/T002R300x300M0000017qM3y4QTwGV.jpg?max_age=2592000',
+        name: 'Hope',
+        author: 'Blazo'
+      }, {
+        src: 'http://dl.stream.qqmusic.qq.com/C40000449CEu3SSxcI.m4a?vkey=15380DA5813F0AD6A7CB754367B9CABB2F061FB2EA35D0AF2DAE466D38EC6026F1AAC944DB38E5C629BB49C8DE7435B18AE273552ABD0BCB&guid=2943454662&uin=2279763407&fromtag=66',
+        poster: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000002jiwiW3WA6te.jpg?max_age=2592000',
+        name: 'Flower Dance',
+        author: 'DJ OKAWARI'
+      }, {
+        src: 'http://dl.stream.qqmusic.qq.com/C400004c1n552tWx2P.m4a?vkey=D901A72F4186B06E115A368D11414D8C0A9D1BC876ED079369E3B68CD4B0187AC5D98532042C83178F6CB6919B223683902E40ECC7E2AA05&guid=2943454662&uin=2279763407&fromtag=66',
+        poster: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000002y4fHI1d6aaj.jpg?max_age=2592000',
+        name: 'Breath And Life',
+        author: 'Audiomachine'
+      }, {
+        src: 'http://dl.stream.qqmusic.qq.com/C400004NaW8V24bvbf.m4a?vkey=E75C8138997A38627ADE9FF3A55E853296EE393FDC36F4DA323010B28446718D8593E7B6303FD73FD4F205CD5AF2408E47BFCAD18D28A087&guid=2943454662&uin=2279763407&fromtag=66',
+        poster: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000001tlCqV0UVXXA.jpg?max_age=2592000  ',
+        name: 'Annabelle',
+        author: '高至豪'
+      }
+    ]
