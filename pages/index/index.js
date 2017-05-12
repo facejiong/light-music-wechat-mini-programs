84ed2a7d9e7442d3fc49870f97a51d0ba22914da
facejiong <2279763407@qq.com>
2017-05-12 09:07:14 +0800

    add slider

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,6 +8,9 @@ Page({
     audioIndex: 0,
     pauseStatus: true,
     listShow: false,
+    timer: '',
+    currentPosition: 0,
+    duration:0,    
   },
   onLoad: function () {
     console.log('onLoad')
@@ -22,7 +25,27 @@ Page({
   onReady: function (e) {
     console.log('onReady')
     // 使用 wx.createAudioContext 获取 audio 上下文 context
-    this.audioCtx = wx.createAudioContext('audio')
+    // this.audioCtx = wx.createAudioContext('audio')
+  },
+  bindSliderchange: function(e) {
+    // clearInterval(this.data.timer)
+    let value = e.detail.value
+    let that = this
+    console.log(e.detail.value)
+    wx.getBackgroundAudioPlayerState({
+      success: function (res) {
+        console.log(res)
+        let {status, duration} = res
+        if (status === 1 || status === 0) {
+          that.setData({
+            sliderValue: value
+          })
+          wx.seekBackgroundAudio({
+              position: value * duration / 100,
+          })
+        }
+      }
+    })
   },
   bindTapPrev: function() {
     console.log('bindTapNext')
@@ -34,11 +57,16 @@ Page({
     } else {
       audioIndexNow = audioIndexPrev - 1
     }
-    this.setData({audioIndex: audioIndexNow})
+    this.setData({
+      audioIndex: audioIndexNow,
+      sliderValue: 0,
+      currentPosition: 0,
+      duration:0, 
+    })
     let that = this
     setTimeout(() => {
       if (that.data.pauseStatus === true) {
-        that.audioCtx.play()
+        that.play()
       }
     }, 1000)
     wx.setStorageSync('audioIndex', audioIndexNow)
@@ -53,11 +81,16 @@ Page({
     } else {
       audioIndexNow = audioIndexPrev + 1
     }
-    this.setData({audioIndex: audioIndexNow})
+    this.setData({
+      audioIndex: audioIndexNow,
+      sliderValue: 0,
+      currentPosition: 0,
+      duration:0, 
+    })
     let that = this
     setTimeout(() => {
       if (that.data.pauseStatus === false) {
-        that.audioCtx.play()
+        that.play()
       }
     }, 1000)
     wx.setStorageSync('audioIndex', audioIndexNow)
@@ -66,10 +99,10 @@ Page({
     console.log('bindTapPlay')
     console.log(this.data.pauseStatus)
     if (this.data.pauseStatus === true) {
-      this.audioCtx.play()
+      this.play()
       this.setData({pauseStatus: false})
     } else {
-      this.audioCtx.pause()
+      wx.pauseBackgroundAudio()
       this.setData({pauseStatus: true})
     }
   },
@@ -90,10 +123,57 @@ Page({
     let that = this
     setTimeout(() => {
       if (that.data.pauseStatus === false) {
-        that.audioCtx.play()
+        that.play()
       }
     }, 1000)
     wx.setStorageSync('audioIndex', parseInt(e.currentTarget.id, 10))
+  },
+  play() {
+    let {audioList, audioIndex} = this.data
+    wx.playBackgroundAudio({
+      dataUrl: audioList[audioIndex].src,
+      title: audioList[audioIndex].name,
+      coverImgUrl: audioList[audioIndex].poster
+    })
+    let that = this
+    let timer = setInterval(function() {
+      that.setDuration(that)
+    }, 1000)
+    this.setData({timer: timer})
+  },
+  setDuration(that) {
+    wx.getBackgroundAudioPlayerState({
+      success: function (res) {
+        console.log(res)
+        let {status, duration, currentPosition} = res
+        if (status === 1 || status === 0) {
+          that.setData({
+            currentPosition: that.stotime(currentPosition),
+            duration: that.stotime(duration),
+            sliderValue: Math.floor(currentPosition * 100 / duration),
+          })
+        }
+      }
+    })
+  },
+  stotime: function(s) {
+    let t = '';
+    if(s > -1) {
+      // let hour = Math.floor(s / 3600);
+      let min = Math.floor(s / 60) % 60;
+      let sec = s % 60;
+      // if (hour < 10) {
+      //   t = '0' + hour + ":";
+      // } else {
+      //   t = hour + ":";
+      // }
+
+      if (min < 10) { t += "0"; }
+      t += min + ":";
+      if (sec < 10) { t += "0"; }
+      t += sec;
+    }
+    return t;
   },
   onShareAppMessage: function () {
     let that = this
